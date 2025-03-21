9300d0cd664c80ab52582cc5be8e62cd209de88e
Aditya Srivastava <adityasrivastava@Adityas-MacBook-Air.local>
2025-03-21 14:30:00 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import { toast, Toaster } from "react-hot-toast"; 
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className='text-red-500'>hi</h1>
-    </>
+    <Router>
+      {/* <Navbar /> */}
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
   )
 }
 
