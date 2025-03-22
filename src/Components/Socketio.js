e8c3622c25970d8355f31919c05ef7ed8bd8fcf3
Aditya Srivastava <adityasrivastava@Adityas-MacBook-Air.local>
2025-03-22 10:04:40 +0530

    manab pardes

--- src/Components/Socketio.js
+++ src/Components/Socketio.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://jeevanya-front-alpha.vercel.app", {
+const socket = io("http://localhost:3000", {
   transports: ["websocket"], // Ensures WebSocket connection
 });
 
